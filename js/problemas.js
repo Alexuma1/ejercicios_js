cb187a0ed56e094f1be9c260caff6925c595f26f
alexuma <m5a97b@gmail.com>
2022-02-18 08:49:06 -0300

    ejercicio 04

--- js/problemas.js
+++ js/problemas.js
@@ -1,9 +1,11 @@
 const texto01 = 'hola mundo';
 const texto02 = 'Hola mundo';
 const texto03 = 'hola que tal';
+const texto04 = 'hola bro ';
 
 export const textos ={
     texto01,
     texto02,
-    texto03
+    texto03,
+    texto04
 }
--- js/resultados.js
+++ js/resultados.js
@@ -21,9 +21,15 @@ const convertirCaracteres = function(texto,separador){
     const arrayTextos = texto.split(separador);
     console.log(arrayTextos);
 }
+const repetidor = function(texto,count){
+    const repetidorTexto = texto.repeat(count);
+    console.log(repetidorTexto);
+}
 esTexto(textos.texto01);
 esTexto(textos.texto02);
 esTexto(textos.texto03);
+esTexto(textos.texto04);
 largoTexto(textos.texto01);
 mostrarCaracteres(textos.texto02);
 convertirCaracteres(textos.texto03," ");
+repetidor(textos.texto04,3);
