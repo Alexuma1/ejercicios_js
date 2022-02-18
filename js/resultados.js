e5d89142db6287b75c41ec00aa3f54a83518ff0b
alexuma <m5a97b@gmail.com>
2022-02-18 09:06:22 -0300

    cambio .log por warm e info

--- js/resultados.js
+++ js/resultados.js
@@ -3,25 +3,25 @@ import { textos } from "./problemas.js";
 const esTexto = function (texto) {
     var tipoDato = typeof texto;
     if (tipoDato === "string") {
-        console.log('Es un texto');
+        console.info('Es un texto');
     }
-     else {
-            console.log(`No es un texto es un: ${tipoDato}`);
-        }
+    else {
+        console.warm(`No es un texto es un: ${tipoDato}`);
+    }
 }
-const largoTexto = function (texto){
+const largoTexto = function (texto) {
     const NumeroCaracteres = texto.length;
     console.log(NumeroCaracteres);
 }
-const mostrarCaracteres = function (texto){
-    const palabraCortada = texto.substr(0,5);
+const mostrarCaracteres = function (texto) {
+    const palabraCortada = texto.substr(0, 5);
     console.log(palabraCortada);
 }
-const convertirCaracteres = function(texto,separador){
+const convertirCaracteres = function (texto, separador) {
     const arrayTextos = texto.split(separador);
     console.log(arrayTextos);
 }
-const repetidor = function(texto,count){
+const repetidor = function (texto, count) {
     const repetidorTexto = texto.repeat(count);
     console.log(repetidorTexto);
 }
@@ -31,5 +31,5 @@ esTexto(textos.texto03);
 esTexto(textos.texto04);
 largoTexto(textos.texto01);
 mostrarCaracteres(textos.texto02);
-convertirCaracteres(textos.texto03," ");
-repetidor(textos.texto04,3);
+convertirCaracteres(textos.texto03, " ");
+repetidor(textos.texto04, 3);
