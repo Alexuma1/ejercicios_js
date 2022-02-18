7bd0a8409ac60ec38959ecbe6889bc982e41c375
alexuma <m5a97b@gmail.com>
2022-02-18 08:44:43 -0300

    ejercicio 03

--- js/problemas.js
+++ js/problemas.js
@@ -1,7 +1,9 @@
 const texto01 = 'hola mundo';
 const texto02 = 'Hola mundo';
+const texto03 = 'hola que tal';
 
 export const textos ={
     texto01,
-    texto02
+    texto02,
+    texto03
 }
--- js/resultados.js
+++ js/resultados.js
@@ -17,7 +17,13 @@ const mostrarCaracteres = function (texto){
     const palabraCortada = texto.substr(0,5);
     console.log(palabraCortada);
 }
+const convertirCaracteres = function(texto,separador){
+    const arrayTextos = texto.split(separador);
+    console.log(arrayTextos);
+}
 esTexto(textos.texto01);
 esTexto(textos.texto02);
+esTexto(textos.texto03);
 largoTexto(textos.texto01);
-mostrarCaracteres(textos.texto02)
+mostrarCaracteres(textos.texto02);
+convertirCaracteres(textos.texto03," ");
