5a620d3e563c5be138afdab1cef7222d5c4aa149
alexuma <m5a97b@gmail.com>
2022-02-18 09:51:01 -0300

    Arreglos al codigo y siguiendo repasando

--- js/problemas.js
+++ js/problemas.js
@@ -1,7 +1,20 @@
+// 1) Programa una función que cuente el número de caracteres de una cadena de texto, pe. miFuncion("Hola Mundo") devolverá 10.
+
 const texto01 = 'hola mundo';
+
+// 2) Programa una función que te devuelva el texto recortado según el número de caracteres indicados, pe. miFuncion("Hola Mundo", 4) devolverá "Hola".
+
 const texto02 = 'Hola mundo';
+
+// 3) Programa una función que dada una String te devuelva un Array de textos separados por cierto caracter, pe. miFuncion('hola que tal', ' ') devolverá ['hola', 'que', 'tal'].
+
 const texto03 = 'hola que tal';
+
+// 4) Programa una función que repita un texto X veces, pe. miFuncion('Hola Mundo', 3) devolverá Hola Mundo Hola Mundo Hola Mundo.
+
 const texto04 = 'hola bro ';
+
+// exportando los textos que eh ingresado, para resolverlos.
 
 export const textos ={
     texto01,
--- js/resultados.js
+++ js/resultados.js
@@ -1,18 +1,21 @@
+// importando los datos de los problemas para su resolucion
+
 import { textos } from "./problemas.js";
 
-const esTexto = function (texto) {
-    var tipoDato = typeof texto;
-    if (tipoDato === "string") {
-        console.info('Es un texto');
-    }
-    else {
-        console.warm(`No es un texto es un: ${tipoDato}`);
-    }
-}
-const largoTexto = function (texto) {
-    const NumeroCaracteres = texto.length;
-    console.log(NumeroCaracteres);
-}
+
+// comprobando si son textos los datos ingresados
+
+const esTexto = (texto="") =>
+(typeof texto != "string")
+    ?console.warn('No es un texto')
+    :(!texto)
+    ?console.warn('El texto esta vacio')
+    :console.info('Es un texto')
+
+// realizando las funciones pedidas en los problemas
+// problema 01
+const largoTexto = (texto) => console.log(`El numero de caracteres es: ${texto.length} de la palabra ${texto}`);
+// problema 02
 const mostrarCaracteres = function (texto) {
     const palabraCortada = texto.substr(0, 5);
     console.log(palabraCortada);
@@ -25,10 +28,16 @@ const repetidor = function (texto, count) {
     const repetidorTexto = texto.repeat(count);
     console.log(repetidorTexto);
 }
+
+// mostrando en consola las resoluciones primero si son textos
+
 esTexto(textos.texto01);
 esTexto(textos.texto02);
 esTexto(textos.texto03);
 esTexto(textos.texto04);
+
+// mostrando en consola las resoluciones segundo las resoluciones a los problemas
+
 largoTexto(textos.texto01);
 mostrarCaracteres(textos.texto02);
 convertirCaracteres(textos.texto03, " ");
