d5f762e1ab7299aab650d2bc62f6addc921285f7
alexuma <m5a97b@gmail.com>
2022-02-18 12:07:22 -0300

    guardado hasta lograr la configuracion adecuada

--- js/problemas.js
+++ js/problemas.js
@@ -4,21 +4,21 @@ const texto01 = 'hola mundo';
 
 // 2) Programa una función que te devuelva el texto recortado según el número de caracteres indicados, pe. miFuncion("Hola Mundo", 4) devolverá "Hola".
 
-const texto02 = 'Hola mundo';
+const texto02 = 'hola mundo';
+const longitud = 4;
 
 // 3) Programa una función que dada una String te devuelva un Array de textos separados por cierto caracter, pe. miFuncion('hola que tal', ' ') devolverá ['hola', 'que', 'tal'].
 
 const texto03 = 'hola que tal';
+const espacio = ' ';
 
 // 4) Programa una función que repita un texto X veces, pe. miFuncion('Hola Mundo', 3) devolverá Hola Mundo Hola Mundo Hola Mundo.
 
 const texto04 = 'hola bro ';
+const contador = 3;
 
 // exportando los textos que eh ingresado, para resolverlos.
 
-export const textos ={
-    texto01,
-    texto02,
-    texto03,
-    texto04
+export const valores ={
+    texto01,texto02,texto03,texto04,longitud,espacio,contador
 }
--- js/resultados.js
+++ js/resultados.js
@@ -1,44 +1,62 @@
 // importando los datos de los problemas para su resolucion
 
-import { textos } from "./problemas.js";
+import { valores } from "./problemas.js";
 
 
-// comprobando si son textos los datos ingresados
+// constantes que comprueban si los datos ingresados son del tipo correcto:
+
+// los datos ingresados son del tipo texto:
 
 const esTexto = (texto="") =>
 (typeof texto != "string")
-    ?console.warn('No es un texto')
+    ?console.warn('No es un texto, por favor ingresa un texto')
     :(!texto)
     ?console.warn('El texto esta vacio')
     :console.info('Es un texto')
 
+// los datos ingresados son del tipo numero:
+
+const esNumero = (numero=undefined) =>
+(typeof numero != 'number')
+    ?console.warn('No es un numero, por favor ingresa un numero')
+    :(numero===0)
+    ?console.warn('Ingresa un numero mayor a 0')
+    :console.info('Es un numero')
+
+// los datos ingresados son del tipo cadena vacia:
+const esEspacio = (texto="") =>
+(typeof texto != "string")
+    ?console.warn('No es un texto, por favor ingresa un texto')
+    :(texto!=' ')
+    ?console.warn('No es un espacio por favor ingresa un espacio en blanco')
+    :console.info('Es un espacio en blanco')
+
+
 // realizando las funciones pedidas en los problemas
-// problema 01
-const largoTexto = (texto) => console.log(`El numero de caracteres es: ${texto.length} de la palabra ${texto}`);
-// problema 02
-const mostrarCaracteres = function (texto) {
-    const palabraCortada = texto.substr(0, 5);
-    console.log(palabraCortada);
-}
-const convertirCaracteres = function (texto, separador) {
-    const arrayTextos = texto.split(separador);
-    console.log(arrayTextos);
-}
-const repetidor = function (texto, count) {
-    const repetidorTexto = texto.repeat(count);
-    console.log(repetidorTexto);
-}
+
+// const largoTexto = (texto) => console.log(`El numero de caracteres es: ${texto.length} de la palabra ${texto}`);
+const mostrarCaracteres = (texto,longitud) => console.log(texto.slice(0,longitud));
+// const convertirCaracteres = (texto, separador) => console.log(texto.split(separador));
+// const repetidor = (texto, count) => console.log(texto.repeat(count));
+
 
 // mostrando en consola las resoluciones primero si son textos
 
-esTexto(textos.texto01);
-esTexto(textos.texto02);
-esTexto(textos.texto03);
-esTexto(textos.texto04);
+
+// const mostrarEnConsola = (esTexto=true&&esNumero==true) => mostrarCaracteres(valores.texto02,valores.longitud);
+esTexto(valores.texto01);
+esTexto(valores.texto02);
+esTexto(valores.texto03);
+esTexto(valores.texto04);
+
+esNumero(valores.longitud);
+esEspacio(valores.espacio);
+esNumero(valores.contador);
+// mostrarEnConsola();
 
 // mostrando en consola las resoluciones segundo las resoluciones a los problemas
 
-largoTexto(textos.texto01);
-mostrarCaracteres(textos.texto02);
-convertirCaracteres(textos.texto03, " ");
-repetidor(textos.texto04, 3);
+largoTexto(valores.texto01);
+mostrarCaracteres(valores.texto02,valores.longitud);
+convertirCaracteres(valores.texto03,valores.espacio);
+repetidor(valores.texto04, valores.contador);
