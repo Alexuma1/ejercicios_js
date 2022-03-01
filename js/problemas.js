f939ac70482b81719eb7109e6b7467f763b3db6b
alexuma <m5a97b@gmail.com>
2022-03-01 14:53:56 -0300

    contar caracteres repaso

--- js/problemas.js
+++ js/problemas.js
@@ -1,22 +1,18 @@
-// 1) Programa una función que cuente el número de caracteres de una cadena de texto, pe. miFuncion("Hola Mundo") devolverá 10.
-
-const texto01 = 'hola mundo';
 
 // 2) Programa una función que te devuelva el texto recortado según el número de caracteres indicados, pe. miFuncion("Hola Mundo", 4) devolverá "Hola".
 
-const texto02 = 'hola mundo';
+const texto02 = "hola mundo";
 const longitud = 6;
 
 // 3) Programa una función que dada una String te devuelva un Array de textos separados por cierto caracter, pe. miFuncion('hola que tal', ' ') devolverá ['hola', 'que', 'tal'].
 
-const texto03 = 'hola que tal como estas tu';
-const separador = ' ';
+const texto03 = "hola que tal como estas tu";
+const separador = " ";
 
 // 4) Programa una función que repita un texto X veces, pe. miFuncion('Hola Mundo', 3) devolverá Hola Mundo Hola Mundo Hola Mundo.
 
-const texto04 = 'hola bro';
+const texto04 = "hola bro";
 const repetidor = 3;
-
 
 // 5) Programa una función que invierta las palabras de una cadena de texto, pe. miFuncion("Hola Mundo") devolverá "odnuM aloH".
 
@@ -24,18 +20,18 @@ const texto05 = "alejandro";
 
 // 6) Programa una función para contar el número de veces que se repite una palabra en un texto largo, pe. miFuncion("hola mundo adios mundo", "mundo") devolverá 2.
 
-const texto06 = "hola mundo adios mundo"
-const palabraRepetida = "mundo"
+const texto06 = "hola mundo adios mundo";
+const palabraRepetida = "mundo";
 
 // 7) Programa una función que valide si una palabra o frase dada, es un palíndromo (que se lee igual en un sentido que en otro), pe. mifuncion("Salas") devolverá true.
 
-const texto07 = "salas operando casa cosa ufa bob"
-let esPalindromo = false
+const texto07 = "salas operando casa cosa ufa bob";
+let esPalindromo = false;
 
 // 8) Programa una función que elimine cierto patrón de caracteres de un texto dado, pe. miFuncion("xyz1, xyz2, xyz3, xyz4 y xyz5", "xyz") devolverá  "1, 2, 3, 4 y 5.
 
-const texto08 = "xyz1 xyz2 xyz3 xyz4 xyz5 xyz6 xyz7 xyz8 xyz"
-const patron = "xyz"
+const texto08 = "xyz1 xyz2 xyz3 xyz4 xyz5 xyz6 xyz7 xyz8 xyz";
+const patron = "xyz";
 
 // 9) Programa una función que obtenga un numero aleatorio entre 501 y 600.
 
@@ -61,84 +57,118 @@ const numeroParImpar = -18;
 // 14) Programa una función para convertir grados Celsius a Fahrenheit y viceversa, pe. miFuncion(0,"C") devolverá 32°F.
 
 const grados = 23;
-const medida = 'f';
+const medida = "f";
 
 // 15) Programa una función para convertir números de base binaria a decimal y viceversa, pe. miFuncion(100,2) devolverá 4 base 10.
 
-const numeroAConvertir = 4
-const base = 10
+const numeroAConvertir = 4;
+const base = 10;
 
 // 16) Programa una función que devuelva el monto final después de aplicar un descuento a una cantidad dada, pe. miFuncion(1000, 20) devolverá 800.
 
-const compra = 1000
-const descuento = 20
-
+const compra = 1000;
+const descuento = 20;
 
 // 17) Programa una función que dada una fecha válida determine cuantos años han pasado hasta el día de hoy, pe. miFuncion(new Date(1984,4,23)) devolverá 35 años (en 2020).
 
-let anio = new Date (1984,4,23)
+let anio = new Date(1984, 4, 23);
 
 // 18) Programa una función que dada una cadena de texto cuente el número de vocales y consonantes, pe. miFuncion("Hola Mundo") devuelva Vocales: 4, Consonantes: 5.
 
-const cadenaTexto = 'hola mundo'
+const cadenaTexto = "hola mundo";
 
 // 19) Programa una función que valide que un texto sea un nombre válido, pe. miFuncion("Jonathan MirCha") devolverá verdadero.
 
-const nombreValido = 'Jonathan MirCha'
+const nombreValido = "Jonathan MirCha";
 
 // 20) Programa una función que valide que un texto sea un email válido, pe. miFuncion("jonmircha@gmail.com") devolverá verdadero.
 
-const emailValido = 'alex@gmail.com'
+const emailValido = "alex@gmail.com";
 
 // 1) Programa una función que dado un array numérico devuelve otro array con los números elevados al cuadrado, pe. mi_funcion([1, 4, 5]) devolverá [1, 16, 25].
 
-const numerosArray = [1,4,5]
+const numerosArray = [1, 4, 5];
 
 // 22) Programa una función que dado un array devuelva el número mas alto y el más bajo de dicho array, pe. miFuncion([1, 4, 5, 99, -60]) devolverá [99, -60].
 
-const arrayAltoBajo = [1,4,5,99,-60]
+const arrayAltoBajo = [1, 4, 5, 99, -60];
 
 // 23) Programa una función que dado un array de números devuelva un objeto con 2 arreglos en el primero almacena los números pares y en el segundo los impares, pe. miFuncion([1,2,3,4,5,6,7,8,9,0]) devolverá {pares: [2,4,6,8,0], impares: [1,3,5,7,9]}.
 
-const arrayParesImpares = [1,2,3,4,5,6,7,8,9,0]
+const arrayParesImpares = [1, 2, 3, 4, 5, 6, 7, 8, 9, 0];
 
 // 24) Programa una función que dado un arreglo de números devuelva un objeto con dos arreglos, el primero tendrá los numeros ordenados en forma ascendente y el segundo de forma descendiente, pe. miFuncion([7, 5,7,8,6]) devolverá { asc: [5,6,7,7,8], desc: [8,7,7,6,5] }.
 
-const numAscDes = [7,5,7,8,6]
+const numAscDes = [7, 5, 7, 8, 6];
 
 // 25) Programa una función que dado un arreglo de elementos, elimine los duplicados, pe. miFuncion(["x", 10, "x", 2, "10", 10, true, true]) devolverá ["x", 10, 2, "10", true].
 
-const duplicados = ['x',10,'x',2,'10',10,true,true]
+const duplicados = ["x", 10, "x", 2, "10", 10, true, true];
 
 // 26) Programa una función que dado un arreglo de números obtenga el promedio, pe. promedio([9,8,7,6,5,4,3,2,1,0]) devolverá 4.5.
 
-const promedio = [9,8,7,6,5,4,3,2,1,0]
+const promedio = [9, 8, 7, 6, 5, 4, 3, 2, 1, 0];
 
 // 27) Programa una clase llamada Pelicula.
 
 // La clase recibirá un objeto al momento de instanciarse con los siguentes datos: id de la película en IMDB, titulo, director, año de estreno, país o países de origen, géneros y calificación en IMBD.
 //   - Todos los datos del objeto son obligatorios.
-//   - Valida que el id IMDB tenga 9 caracteres, los primeros 2 sean letras y los 
+//   - Valida que el id IMDB tenga 9 caracteres, los primeros 2 sean letras y los
 //      7 restantes números.
 //   - Valida que el título no rebase los 100 caracteres.
 //   - Valida que el director no rebase los 50 caracteres.
 //   - Valida que el año de estreno sea un número entero de 4 dígitos.
 //   - Valida que el país o paises sea introducidos en forma de arreglo.
 //   - Valida que los géneros sean introducidos en forma de arreglo.
-//   - Valida que los géneros introducidos esten dentro de los géneros 
+//   - Valida que los géneros introducidos esten dentro de los géneros
 //      aceptados*.
 //   - Crea un método estático que devuelva los géneros aceptados*.
-//   - Valida que la calificación sea un número entre 0 y 10 pudiendo ser 
+//   - Valida que la calificación sea un número entre 0 y 10 pudiendo ser
 //     decimal de una posición.
 //   - Crea un método que devuelva toda la ficha técnica de la película.
-//   - Apartir de un arreglo con la información de 3 películas genera 3 
-//     instancias de la clase de forma automatizada e imprime la ficha técnica 
+//   - Apartir de un arreglo con la información de 3 películas genera 3
+//     instancias de la clase de forma automatizada e imprime la ficha técnica
 //     de cada película.
 
 // * Géneros Aceptados: Action, Adult, Adventure, Animation, Biography, Comedy, Crime, Documentary ,Drama, Family, Fantasy, Film Noir, Game-Show, History, Horror, Musical, Music, Mystery, News, Reality-TV, Romance, Sci-Fi, Short, Sport, Talk-Show, Thriller, War, Western.
 
 // exportando los textos que eh ingresado, para resolverlos.
 
-export const valores ={
-    texto01,texto02,texto03,texto04,longitud,separador,repetidor,texto05,texto06,palabraRepetida,texto07,esPalindromo,texto08,patron,minimo,maximo,numero,numeroFactorial,numeroPrimo,numeroParImpar,grados,medida,numeroAConvertir,base,compra,descuento,anio,cadenaTexto,nombreValido,emailValido,numerosArray,arrayAltoBajo,arrayParesImpares,numAscDes,duplicados,promedio
-}
+export const valores = {
+  texto01,
+  texto02,
+  texto03,
+  texto04,
+  longitud,
+  separador,
+  repetidor,
+  texto05,
+  texto06,
+  palabraRepetida,
+  texto07,
+  esPalindromo,
+  texto08,
+  patron,
+  minimo,
+  maximo,
+  numero,
+  numeroFactorial,
+  numeroPrimo,
+  numeroParImpar,
+  grados,
+  medida,
+  numeroAConvertir,
+  base,
+  compra,
+  descuento,
+  anio,
+  cadenaTexto,
+  nombreValido,
+  emailValido,
+  numerosArray,
+  arrayAltoBajo,
+  arrayParesImpares,
+  numAscDes,
+  duplicados,
+  promedio,
+};
