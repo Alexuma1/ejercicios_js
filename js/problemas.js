048b7135870553ed0789109d99dcb00e5e5af7a0
alexuma <m5a97b@gmail.com>
2022-03-02 12:02:19 -0300

    eliminar caracteres repaso

--- js/problemas.js
+++ js/problemas.js
@@ -1,8 +1,3 @@
-// 7) Programa una función que valide si una palabra o frase dada, es un palíndromo (que se lee igual en un sentido que en otro), pe. mifuncion("Salas") devolverá true.
-
-const texto07 = "salas operando casa cosa ufa bob";
-let esPalindromo = false;
-
 // 8) Programa una función que elimine cierto patrón de caracteres de un texto dado, pe. miFuncion("xyz1, xyz2, xyz3, xyz4 y xyz5", "xyz") devolverá  "1, 2, 3, 4 y 5.
 
 const texto08 = "xyz1 xyz2 xyz3 xyz4 xyz5 xyz6 xyz7 xyz8 xyz";
