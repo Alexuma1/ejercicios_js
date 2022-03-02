d93eeb8afb3737d02957e7b7a242c3e21e340793
alexuma <m5a97b@gmail.com>
2022-03-02 12:42:44 -0300

    capicua repaso

--- js/problemas.js
+++ js/problemas.js
@@ -1,8 +1,6 @@
 const minimo = 501;
 const maximo = 601;
 
-// 10) Programa una función que reciba un número y evalúe si es capicúa o no (que se lee igual en un sentido que en otro), pe. miFuncion(2002) devolverá true.
-
 const numero = 505;
 
 // 11) Programa una función que calcule el factorial de un número (El factorial de un entero positivo n, se define como el producto de todos los números enteros positivos desde 1 hasta n), pe. miFuncion(5) devolverá 120.
